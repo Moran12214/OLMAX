faff85b03c07182e437424a0091c0660bb90db4f
moran12214 <moranchik123@gmail.com>
2026-04-19 22:11:58 +0200

    888

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import psycopg2
 import os
 import time
